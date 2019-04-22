22f869cea9c7effe72bf41f4bba0a77e17e5d254
Ananda Prabawa <ananda.widiprabawa@gmail.com>
2019-04-22 19:59:32 +0800

    Change login to signin route name

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from '~/routes/middlewares/AuthGuard';
 export const routes = (router: Router) => {
   router.get('/auth/current', AuthGuard, AuthController.CurrentAuth);
   router.post('/auth/local/signup', AuthController.SignUpWithEmail);
-  router.post('/auth/local/login', AuthController.SignInWithEmail);
+  router.post('/auth/local/signin', AuthController.SignInWithEmail);
   router.post('/auth/local/forgot-password', AuthController.ForgotPassword);
   router.post(
     '/auth/local/check-password-token',
